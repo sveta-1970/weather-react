0cfc427692a0e4ba55d24ea0543afe0517402382
sveta-1970 <ssarisoydan@gmail.com>
2023-07-15 22:49:16 +0300

    Added axios integration so it shows weather of the city

---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${props.city} is ${Math.round(response.data.main.temp)}°C`
+    );
+  }
+  let apiKey = "52b789a53d83895c9bd9e318a67b4fa8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="#4fa94d"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{}}
+      wrapperClassName=""
+      visible={true}
+    />
+  );
+}
