d6ce4dd9c7cf9d09f96921566446b7e38744c173
sveta-1970 <ssarisoydan@gmail.com>
2023-07-20 23:41:18 +0300

    Updated footer, deleted unnecessary files

--- src/Footer.js
+++ src/Footer.js
@@ -4,14 +4,18 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <small>
+      This project was coded by{" "}
+      <a href="https://www.linkedin.com/in/svitlana-shkriabina-5a2072157/">
+        Svitlana Shkriabina
+      </a>{" "}
+      and is{" "}
       <a
         href="https://github.com/sveta-1970/weather-react"
         target="_blank"
         rel="noreferrer"
       >
-        Open-source code
+        open-sourced on GitHub
       </a>
-      by Svitlana Shkriabina
     </small>
   );
 }
