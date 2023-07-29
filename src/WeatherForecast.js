539307bca76d149034d0322cb942b83495aef8a0
sveta-1970 <ssarisoydan@gmail.com>
2023-07-29 19:33:17 +0300

    Created layout for 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,9 +26,17 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map((dailyForecast, index) => {
+            if (index > 0 && index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
