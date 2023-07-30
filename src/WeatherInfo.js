cc7ee769fd4754702566de01161b45bec547b33a
sveta-1970 <ssarisoydan@gmail.com>
2023-07-30 10:22:16 +0300

    Deleted farenheit temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,25 +1,33 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
+      <div className="main">
+        <h1>{props.data.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+      </div>
       <div className="row mt-3">
         <div className="col-6">
           <div className="float">
-            <WeatherIcon code={props.data.icon} alt={props.data.description} size={64}/>
+            <WeatherIcon
+              code={props.data.icon}
+              alt={props.data.description}
+              size={64}
+            />
           </div>
-          <div>
-            <WeatherTemperature celsius={props.data.temperature} />
+          <div className="WeatherTemperature">
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit">°C</span>
           </div>
         </div>
         <div className="col-6">
